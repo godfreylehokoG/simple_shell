485bcab2b8e701c2dac417b3a5a170f36c532e5e
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-05-16 21:24:46 +0200

    helper functions [1]

---
+++ _itoa.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+void _itoa(int i, char *t)
+{
+	int j;
+	int rem = 0;
+	int counter = 0;
+	char temp;
+
+	if (i == 0)
+	{
+		t[0] = '0';
+		t[1] = '\0';
+	}
+
+	while (i != 0)
+	{
+		rem = i % 10;
+		t[counter++] = rem + '0';
+		i = i / 10;
+	}
+
+	for (j = 0; j < counter / 2; j++)
+	{
+		temp = t[j];
+		t[j] = t[counter - 1 - j];
+		t[counter - 1 - j] = temp;
+	}
+
+	t[counter] = '\0';
+
+}
