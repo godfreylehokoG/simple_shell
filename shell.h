0cc32439b5f6e212aa8d3e976ad0ee89f56ffbd7
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-05-17 18:14:21 +0200

    testing phase [7]

--- shell.h
+++ shell.h
@@ -12,21 +12,24 @@
 #include <sys/types.h>
 
 /**
- * * struct chdirect - struct to implement cd builtin
- * * @s: directory path
- * * @boo: boolean to indicate if cd was called
- * */
+ * struct chdirect - struct to implement cd builtin
+ * @s: directory path
+ * @boo: boolean to indicate if cd was called
+ */
+
 typedef struct chdirect
 {
 	char *s;
 	int boo;
 
 } CHDIRECT;
+
 /**
- * * struct direc - struct to hold linked list of PATH values
- * * @s: directory path
- * * @next: pointer to next node
- * */
+ * struct direc - struct to hold linked list of PATH values
+ * @s: directory path
+ * @next: pointer to next node
+ */
+
 typedef struct direc
 {
 	char *s;
@@ -35,9 +38,9 @@ typedef struct direc
 } PDIRECT;
 extern char **environ;
 
+int _putchar(char c);
 int _strlen(char *s);
 int _atoi(char *s);
-void _itoa(int i, char *t);
 char *_strdup(char *s);
 int _strcmp(char *s1, char *s2);
 int tokencount(char *s);
